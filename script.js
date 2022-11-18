2d9ac79e829c31f479257af2ce8b6bdf284d2378
Aidan <90624501+aidankiely@users.noreply.github.com>
2022-11-18 14:09:17 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function myFunction() {
+ 
+  var x = document.getElementById("states");
+  var i = x.selectedIndex;
+  var b = x.value;
+  document.getElementById("state-heading").innerHTML = x.options[i].text;
+  document.getElementById("greeting").innerHTML = "Greetings from";
+  document.getElementById("postcard").style.backgroundImage = "url(img/" + b + "@2x.jpg";
+ 
+}
+
